03160f6a3de09c87006b504c388f9684fff62b67
martel782 <171wita10@o365.htl-leoben.at>
2018-03-05 16:45:29 +0100

    Neues Programm

---
+++ einfache for-schleife.py
@@ -0,0 +1,11 @@
+for i in range(1,10):
+    print("Hello ",end=",")
+    name = "Willkommen an der HTL Leoben"
+    print
+    print(name)
+
+    for z in name:
+        print("=",end="")
+        for z in name:
+            print("="end="")
+print
